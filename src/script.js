da2ca56c9099afd6ce206b987ca1f0a5167ca3b7
Jose Antonio <87820065+Josean001@users.noreply.github.com>
2025-01-14 11:26:38 +0100

    Create repository

---
+++ src/script.js
@@ -0,0 +1,44 @@
+function countLinks() {
+  const links = document.querySelectorAll("a");
+  document.getElementById(
+    "result"
+  ).textContent = `Número total de enlaces: ${links.length}`;
+}
+
+function secondLastLink() {
+  const links = document.querySelectorAll("a");
+  if (links.length >= 2) {
+    document.getElementById(
+      "result"
+    ).textContent = `La dirección del penúltimo enlace es: ${
+      links[links.length - 2].href
+    }`;
+  } else {
+    document.getElementById("result").textContent =
+      "No hay suficientes enlaces para mostrar el penúltimo.";
+  }
+}
+
+function countTwitterLinks() {
+  const links = document.querySelectorAll("a");
+  const twitterLinks = Array.from(links).filter(
+    (link) => link.href === "https://www.twitter.com"
+  );
+  document.getElementById(
+    "result"
+  ).textContent = `Número de enlaces a Twitter: ${twitterLinks.length}`;
+}
+
+function linksInThirdParagraph() {
+  const paragraphs = document.querySelectorAll("#información p");
+  const thirdParagraph = paragraphs[2];
+  if (thirdParagraph) {
+    const links = thirdParagraph.querySelectorAll("a");
+    document.getElementById(
+      "result"
+    ).textContent = `Número de enlaces en el tercer párrafo: ${links.length}`;
+  } else {
+    document.getElementById("result").textContent =
+      "El tercer párrafo no existe.";
+  }
+}
